9e1fd03b8a1023f6a6d5cd028c84634d39a606d4
NikolaStoilkovski17 <tamarajakimovska007@gmail.com>
2023-07-12 12:48:08 +0200

    Added Testimonial

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css"
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Work from "./Components/Work";
+import Testimonial from "./Components/Testimonial";
 
 
 
@@ -11,6 +12,7 @@ return <div className="App">
     <Home />
     <About />
     <Work />
+    <Testimonial />
   </div>
 }
 
